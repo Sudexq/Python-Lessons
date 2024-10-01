6cfc520f2273e7cd992485586d83019d4c86e43c
SudenurTilla <121225010+Sudexq@users.noreply.github.com>
2024-10-01 16:15:20 +0200

    learning lists

---
+++ 9-Lists.py
@@ -0,0 +1,7 @@
+friends = ["Ali", "Ayse", "Zeynep"]
+# index:     0       1        3
+# index:    -3      -2       -1
+
+print(friends)  # ['Ali', 'Ayse', 'Zeynep']
+print(friends[0])  # Ali
+print(friends[-2])  # Ayse
