21381d3d1f80eea5a826aecd8d0984fe6e248d0d
SudenurTilla <121225010+Sudexq@users.noreply.github.com>
2024-10-15 18:48:18 +0200

    while loop

---
+++ 18-While.py
@@ -0,0 +1,21 @@
+
+i = 1 #starting from 1
+while i <= 10: #until i equal to 10
+    print(i)
+    i = i + 1 #increasing i in every loop
+    #check is i smaller than or equal to 10 again
+    #if it is bigger than 10, then go out from loop
+"""
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+"""
+
+
